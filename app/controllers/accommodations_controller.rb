1bde421fcdfdc8bea102208a73bab2f6716ea033
Amar Slaoua <amar.slaoua@gmail.com>
2023-06-07 11:19:26 +0200

    added edit button in show

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -14,16 +14,36 @@ class AccommodationsController < ApplicationController
   end
 
   def edit
+    @accommodation = current_user.accommodation
   end
 
   def create
   end
 
   def update
-
+    @accommodation = current_user.accommodation
+    if @accommodation.update(accommodation_params)
+      redirect_to @accommodation, notice: "Les informations du logement ont été mises à jour."
+    else
+      render :edit
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def accommodation_params
+    params.require(:accommodation).permit(:title,
+                                          :description,
+                                          :nb_of_guests,
+                                          :nb_of_bathrooms,
+                                          :nb_of_bedrooms,
+                                          :nb_of_beds,
+                                          :garden,
+                                          :swimming_pool,
+                                          :balcony)
   end
 end
